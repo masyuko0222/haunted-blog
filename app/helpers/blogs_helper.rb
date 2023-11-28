1b73612004c71e275238b161335222bba5982c3f
masyuko0222 <masyuko0222@gmail.com>
2023-11-28 22:04:37 +0900

    🐛 ブログ投稿でXSSを 起こさないように

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,6 +2,7 @@
 
 module BlogsHelper
   def format_content(blog)
-    blog.content.gsub("\n", '<br>').html_safe # rubocop:disable Rails/OutputSafety
+    # https://qiita.com/mmaumtjgj/items/1b672f3accd37387b2f3#h%E3%82%AA%E3%83%97%E3%82%B7%E3%83%A7%E3%83%B3
+    simple_format(h(blog.content))
   end
 end
